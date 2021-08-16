e34e16c344dc2be74c559a3bce3ff6236b744cc4
bhy0426 <bhy246@naver.com>
2021-08-16 23:43:02 +0900

    20210816

---
+++ week/week.js
@@ -0,0 +1,21 @@
+function javascript1() {
+  document.getElementById("box1").style.backgroundColor = "#67E80C";
+}
+function javascript2() {
+  document.getElementById("box2").style.backgroundColor = "#67E80C";
+}
+function javascript3() {
+  document.getElementById("box3").style.backgroundColor = "#67E80C";
+}
+function javascript4() {
+  document.getElementById("box4").style.backgroundColor = "#67E80C";
+}
+function javascript5() {
+  document.getElementById("box5").style.backgroundColor = "#67E80C";
+}
+function javascript6() {
+  document.getElementById("box6").style.backgroundColor = "#67E80C";
+}
+function javascript7() {
+  document.getElementById("box7").style.backgroundColor = "#67E80C";
+}
