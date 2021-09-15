9e88636e68d03dd32d96ab2a6717bba80b3536bb
qusgkdus <bhy246@naver.com>
2021-09-16 01:07:13 +0900

    20210916

---
+++ covid19/script.js
@@ -0,0 +1,18 @@
+var koreanc = 0;
+//var englishc = 0;
+//var mathc = 0;
+//var societyc = 0;
+//var japanesec = 0;
+var korean = document.getElementById("koreancheckbox");
+var koreanresult = korean.getAttribute("checked");
+function koreancheck()
+{
+    koreanc = 1;
+}
+function koreanload()
+{
+    if (koreanc == 1);
+    {
+        var 
+    }
+}
